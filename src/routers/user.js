f93e144cb77a8e690788fe00467978d71f29c73a
Ilyas <i.abdirahman@gmail.com>
2020-02-01 16:34:36 +0000

    107. Generating Authentication Tokens modified:   src/models/user.js modified:   src/routers/user.js

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,12 +2,13 @@ const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
 
-router.post('/sign-up', async (req, res) => {
+router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
@@ -16,7 +17,8 @@ router.post('/sign-up', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
         res.status(400).send()
     }
